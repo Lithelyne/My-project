c76c9ca800b9e65d78bca2a1bdcf0615b197ef30
Lithelyne <126444415+Lithelyne@users.noreply.github.com>
2023-06-28 12:15:22 -0700

    Animation Update

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,19 +4,60 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    //variable to improve performance by not making you load Rigid over and over.  Put outside start and Update so accessable by inner code
+    private Rigidbody2D rigid;
+    private Animator ani;
+    private SpriteRenderer sprite;
+    private float directionX = 0f;
+    private float moveSpeed = 7f; 
+    private float jumpForce = 14f;
+
+    // Start is called before the first frame update we private as they are only useful in the script
+    private void Start()
     {
-        Debug.Log("Hello,World");
+        rigid = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update is called once per frame we private as they are only useful in the script
+    private void Update()
     {
-        if (Input.GetKeyDown("space"))
+        //Joystick or sensitivity support.  GetAxis has sliding, get axis raw does not
+        directionX = Input.GetAxisRaw("Horizontal");
+        rigid.velocity = new Vector2(directionX * 7f, rigid.velocity.y);
+
+
+
+
+        //using GetButton uses Unity's input manager
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+            rigid.velocity = new Vector2(rigid.velocity.x, 14f);
         }
 
+        UpdateAnimation();
+
+
+    }
+    //making a new method for better organization
+    private void UpdateAnimation()
+    {
+        //left right movement
+        if (directionX > 0f)
+        {
+            ani.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (directionX < 0f)
+        {
+            ani.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            ani.SetBool("running", false);
+        }
     }
 }
